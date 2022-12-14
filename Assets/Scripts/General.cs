ca9101631a4ab9e503f40a6f5fc26e7328840390
MY-Chris <mayuan1999@qq.com>
2022-12-13 17:54:16 -0800

    modified general.cs to fit network settings

--- Assets/Scripts/General.cs
+++ Assets/Scripts/General.cs
@@ -15,6 +15,10 @@ public class General : MonoBehaviour
     void Start()
     {
         Debug.Assert(magic != null);
+        Camera.main.GetComponent<CamController>().target = transform;
+        GameObject.Find("SmallViewCamera").GetComponent<SmallViewCamera>().general = root;
+        GameObject.Find("Canvas").GetComponent<FightMenu>().playerObject = gameObject;
+        GameObject.Find("Canvas").GetComponent<FightMenu>().battleSceneRef = transform.GetComponent<BattleScene>();
     }
 
     // Update is called once per frame
